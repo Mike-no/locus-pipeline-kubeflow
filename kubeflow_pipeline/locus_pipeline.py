c99215684ca9a29a07daf5421a2fac49759f75f4
Mike-no <deangelismichael98@gmail.com>
2021-12-12 02:41:20 +0100

    updated pipeline
    Former-commit-id: 645fbd4872b2d2c5c0bd4aac5927cecee9adae0f

--- kubeflow_pipeline/locus_pipeline.py
+++ kubeflow_pipeline/locus_pipeline.py
@@ -6,18 +6,38 @@ downloader_op = kfp.components.load_component_from_file(os.path.join('./pandas_l
 pandas_loading_op = kfp.components.load_component_from_file(os.path.join('./pandas_loading/', 'component.yaml'))
 numpy_preprocessing_op = kfp.components.load_component_from_file(os.path.join('./numpy_preprocessing/', 'component.yaml'))
 tensorflow_training_op = kfp.components.load_component_from_file(os.path.join('./tensorflow_training/', 'component.yaml'))
+model_storing_op = kfp.components.load_component_from_file(os.path.join('./model_storing/', 'component.yaml'))
 kmeans_clustering_op = kfp.components.load_component_from_file(os.path.join('./kmeans_clustering/', 'component.yaml'))
 
 @kfp.dsl.pipeline(
     name = 'Pandas Pipeline',
     description = 'Locus Pipeline (Pandas Loading)'
 )
-def pandas_pipeline(data_url):
-    downloader = downloader_op(url = data_url)
-    pandas_loading = pandas_loading_op(input_path = downloader.output)
+def pandas_pipeline(data_url, minio_url, minio_access_key, minio_secret_key, graph_url):
+    
+    # Download dataset
+    downloader_dataset = downloader_op(url = data_url)
+
+    # Downlod graph deployment file
+    downloader_graph = downloader_op(url = graph_url)    
+    
+    # Preprocess and Train encoder model, produce embeddings
+    pandas_loading = pandas_loading_op(input_path = downloader_dataset.output)
     numpy_preprocessing = numpy_preprocessing_op(input_path = pandas_loading.output)
     tensorflow_training = tensorflow_training_op(input_path = numpy_preprocessing.output)
+    
+    # Save encoder model in PV
+    encoder_model_storing = model_storing_op(model_path = tensorflow_training.outputs['output_path'], minio_url = minio_url, 
+        minio_access_key = minio_access_key, minio_secret_key = minio_secret_key, graph_path = downloader_graph.output)
+    
+    # Produce clustering model
     kmeans_clustering = kmeans_clustering_op(input_path = tensorflow_training.outputs['output_path2'])
+    
+    # Save clustering model in PV
+    clustering_model_storing = model_storing_op(model_path = kmeans_clustering.output, minio_url = minio_url,
+        minio_access_key = minio_access_key, minio_secret_key = minio_secret_key, graph_path = downloader_graph.output)
+
+    # Start Seldon Deployment for encoder
 
 if __name__ == '__main__':
     import kfp.compiler as compiler
