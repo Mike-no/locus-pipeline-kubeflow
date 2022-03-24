9c4266e1dec6a202cdb20fcdac30e36c3701059b
Mike-no <deangelismichael98@gmail.com>
2022-03-24 16:47:49 +0100

    updated graph test
    Former-commit-id: b9a5f7d20e4c4a364d772092062b861c26717a2c

--- graph/test.py
+++ graph/test.py
@@ -3,7 +3,7 @@ import numpy as np
 import json
 import requests
 
-dataset = tf.data.TFRecordDataset('data_testdata.tfrecord').map(lambda x: tf.io.parse_tensor(x, tf.float32))
+dataset = tf.data.TFRecordDataset('testdata.tfrecord').map(lambda x: tf.io.parse_tensor(x, tf.float32))
 sample = dataset.take(1)
 sample = list(sample.as_numpy_iterator())[0]
 print(type(sample[0][0][0]))
@@ -18,5 +18,5 @@ print(type(loaded[0][0][0]))
 
 request_body = { 'data': { 'ndarray': to_predict } }
 
-response = requests.post('http://10.106.18.124/seldon/models/locus-graph/api/v1.0/predictions', json = request_body)
+response = requests.post('http://10.30.5.20:32353/api/v1.0/predictions', json = request_body)
 print(response.status_code, response.text)
